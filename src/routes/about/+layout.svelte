37e5ee854783cd30014aa7f3d38c8fc5f6f65cec
Web-serfer <redibedi2019@gmail.com>
2022-11-20 01:04:28 +0500

    Commit 19/11/22

---
+++ src/routes/about/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+  import "../app.css";
+  import Header from "../libs/components/Header.svelte";
+  import Footer from "../libs/components/Footer.svelte";
+</script>
+
+<div class="bg-slate-100">
+  <Header />
+    <slot />
+  <Footer />
+</div>
+
